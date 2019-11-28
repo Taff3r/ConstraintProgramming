32f8e9d92b37a1a59512a238a4e7cdb6f05d21df
simon <simon.tenggren@gmail.com>
2019-11-28 14:46:07 +0100

    work work

--- Truck/src/JohnTheTruckDriver.java
+++ Truck/src/JohnTheTruckDriver.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
 
+import org.jacop.constraints.Assignment;
 import org.jacop.constraints.Constraint;
+import org.jacop.constraints.IfThenElse;
 import org.jacop.constraints.LinearInt;
+import org.jacop.constraints.Or;
 import org.jacop.constraints.PrimitiveConstraint;
 import org.jacop.constraints.Reified;
 import org.jacop.constraints.Sum;
 import org.jacop.constraints.SumInt;
+import org.jacop.constraints.XeqC;
+import org.jacop.constraints.XeqY;
 import org.jacop.constraints.XgtC;
 import org.jacop.constraints.XmulCeqZ;
 import org.jacop.constraints.XplusYeqC;
@@ -24,6 +29,7 @@ import org.jacop.search.IndomainMin;
 import org.jacop.search.Search;
 import org.jacop.search.SelectChoicePoint;
 import org.jacop.search.SimpleSelect;
+import org.jacop.search.SmallestDomain;
 
 public class JohnTheTruckDriver {
 	public static void main(String[] args) {
@@ -42,16 +48,18 @@ public class JohnTheTruckDriver {
 	static void solve(int graphSize, int start, int nDests, int nEdges, int[] from, int[] to, int[] cost, int[] dest) {
 		Store store = new Store();
 		NetworkBuilder builder = new NetworkBuilder();
-		BooleanVar[] usedEdges = new BooleanVar[nEdges];
+		IntVar[] usedEdges = new IntVar[nEdges];
 		Node[] nodes = new Node[graphSize];
 		IntVar[] flows = new IntVar[nEdges];
 
 		for (int i = 0; i < nEdges; i++) {
-			flows[i] = new IntVar(store, "flow" + i, 0, Integer.MAX_VALUE);
-			usedEdges[i] = new BooleanVar(store, ("edge" + i) + ("cost=" + cost[i]));
+			flows[i] = new IntVar(store, "flow" + i, 0, 1000);
+			usedEdges[i] = new IntVar(store, ("edge" + i) + ("cost=" + cost[i]), 0, 1); // An edge can either be used or
+																						// not (boolean)
 		}
 
-		nodes[start - 1] = builder.addNode("node" + start + "  (source)", nDests);
+		nodes[start - 1] = builder.addNode("node" + start + "  (source)", nDests); // Balance = nDest (Production from
+																					// this node)
 		// Add all Nodes but the source node
 		for (int i = 0; i < graphSize; i++) {
 			if (i + 1 != start) {
@@ -59,36 +67,54 @@ public class JohnTheTruckDriver {
 																	// producing or consuming)
 			}
 		}
-		IntVar networkCost = new IntVar(store, "flowcost", 0, 1000); // Not used in this assignment to determine cost
-		builder.setCostVariable(networkCost);
-		Arc[] arcs = new Arc[nEdges];
+		Arc[] arcsForward = new Arc[nEdges];
 		// Add arcs with corresponding costs to the network
 		for (int i = 0; i < nEdges; i++) {
-			arcs[i] = builder.addArc(nodes[from[i] - 1], nodes[to[i] - 1], cost[i], flows[i]);
-			store.impose(new Reified(new XgtC(flows[i], 0), usedEdges[i]));
+			arcsForward[i] = builder.addArc(nodes[from[i] - 1], nodes[to[i] - 1], cost[i], flows[i]);
+			PrimitiveConstraint ifX = new XgtC(flows[i], 0);
+			PrimitiveConstraint thenX = new XeqC(usedEdges[i], 1);
+			PrimitiveConstraint elseX = new XeqC(usedEdges[i], 0);
+			store.impose(new IfThenElse(ifX, thenX, elseX));
 		}
 
+		// Make bidirectional
+//		Arc[] arcsBackward = new Arc[nEdges];
+//		for (int i = 0; i < nEdges; i++) {
+//			arcsBackward[i] = builder.addArc(nodes[to[i] - 1], nodes[from[i] - 1], cost[i], flows[i]);
+//			store.impose(new Reified(new XeqC(flows[i], 1), usedEdges[i]));
+//		}
+//		
 		// Use linear int to figure out the cost flow[i] * cost[i]
 		// Add a dummy sink
 		Node dummySink = builder.addNode("sink", -nDests);
 		// Add arcs with cost 0 from the real sink nodes to the dummy sink node
 		for (int sink : dest) {
-			builder.addArc(nodes[sink - 1], dummySink, 0, 0, Integer.MAX_VALUE);
+			builder.addArc(nodes[sink - 1], dummySink, 0, 0, 1); // Add arcs to the sink. Does not have to be
+																		// bidirectional (?)
 		}
 		// How to use LinearInt instead?
-		IntVar costVar = new IntVar(store, "costVar", 0, Integer.MAX_VALUE);
-		IntVar[] costs = new IntVar[nEdges];
+		IntVar costVar = new IntVar(store, "costVar", 0, 100); // Our cost variable
+		builder.setCostVariable(costVar); // Maybe not?
+		IntVar[] costs = new IntVar[nEdges]; // All costs
 		for (int i = 0; i < nEdges; i++) {
-			costs[i] = new IntVar(store, "cost" + i, 0, 100);
-			store.impose(new XmulCeqZ(usedEdges[i], cost[i], costs[i]));
+			costs[i] = new IntVar(store, "cost" + i, 0, 1000);
+			store.impose(new XmulCeqZ(usedEdges[i], cost[i], costs[i])); // Cost[i] = Edge_used(0|1) * cost of arc(a
+																			// weight)
 		}
 		store.impose(new NetworkFlow(builder));
-		store.impose(new SumInt(costs, "==", costVar));
+		store.impose(new Sum(costs, costVar)); // Define the value of costVar to be the sum of all costs.
+		//System.out.println(store.toString());
+		
 		Search<IntVar> search = new DepthFirstSearch<IntVar>();
-		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(usedEdges, null, new IndomainMin<IntVar>());
-		boolean res = search.labeling(store, select, costVar);
+		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(usedEdges, new SmallestDomain<IntVar>(),
+				new IndomainMin<IntVar>());
+		System.out.println(store.toString());
+		search.labeling(store, select, costVar);
+		System.out.println(Arrays.asList(usedEdges));
+		System.out.println(Arrays.asList(flows));
+		
+		System.out.println(Arrays.asList(arcsForward));
 		System.out.println(costVar);
-		System.out.println(Arrays.asList(costs));
 	}
 
 }
